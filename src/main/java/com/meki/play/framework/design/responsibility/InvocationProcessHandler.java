0307e7ff7340881260e23e7e5f798efc05316dab
xujinchao <xujinchao@baidu.com>
2017-08-29 16:14:26 +0800

    test

--- src/main/java/com/meki/play/framework/design/responsibility/InvocationProcessHandler.java
+++ src/main/java/com/meki/play/framework/design/responsibility/InvocationProcessHandler.java
@@ -7,21 +7,17 @@ public abstract class InvocationProcessHandler {
 
     public InvocationProcessHandler nextHandler;
 
-    public void setNextHandler(InvocationProcessHandler handler){
+    public void setNextHandler(InvocationProcessHandler handler) {
         this.nextHandler = handler;
     }
 
     /**
      * 是否可处理该请求
-     * @param request
-     * @return
      */
     public abstract boolean canAccept(InvocationRequest request);
 
     /**
      * 请求处理方法  可将是否可处理的判断放进来
-     * @param request
-     * @return
      */
     public abstract InvocationRequest process(InvocationRequest request);
 
